51228b5c56ba2d647cbe29f1733846c5cfad863b
robertgsimonson <robertgsimonson@gmail.com>
2016-10-11 15:47:27 -0500

    Homework1 complete!

--- Homework1/Homework1/Program.cs
+++ Homework1/Homework1/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
-namespace Homework1
+class MyLittleProject
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        Console.WriteLine("Hello World!");
+
+        Console.ReadLine();
     }
 }
