1815ba44e8f137af89df3d26bd1c87fe10c4e4e6
robertgsimonson <robertgsimonson@gmail.com>
2016-10-20 16:29:02 -0500

    Homework 3 done!

--- Homework3/Homework3/Program.cs
+++ Homework3/Homework3/Program.cs
@@ -12,37 +12,36 @@ namespace Homework3
         {
             Console.WriteLine("Please enter the integer you would like to calculate the factorial for...");
 
-            string userInput = Console.ReadLine();  //intilize userEntry with user input
+            string userInput = Console.ReadLine();  //intilize userEntry with user kybd input
             int userInt;  //Storage for parse value of userEntry
-            int sum = 1;  //Storage for partial result of the iterative factorial calculation.  Assign value of 1 since the
-            
+            int sum = 1;  //Storage for partial result of the iterative factorial calculation.
+
             if (int.TryParse(userInput, out userInt)) //if string parses to an int:  store in userInt
             {
-                if (userInt >= 0)  //Check for a negative int 
+                if (userInt == 0) //Check for 0
                 {
-                    if (userInt == 0) //Check for 0
-                    {
-                        Console.WriteLine("The Factorial of 0 is defined as 1");
-                        Console.ReadLine();
-                    }
-                    else  //Positive nonzero int was entered.  Proceed with calculation
-                    {
-                        for (; userInt > 0; userInt--)
-                        {
-                            sum = sum * userInt;   //accumulate the sum of the iterations to find factorial.  First interation sets sum to userInt.  Covers rule 1! = 1
-                        }
-                        Console.WriteLine("The Factorial of " + userInput + " is " + sum);
-                        Console.ReadLine();
-                    }
+                    Console.WriteLine("The Factorial of 0 is defined as 1");
+                    Console.ReadLine();
                 }
-                else  //user entered negative int
+
+                else if (userInt < 0)  //check for negative integer
                 {
                     Console.WriteLine("The factorial of a negative integer is undefined");
                     Console.ReadLine();
                 }
 
+                else //positive integer!!!  Find factorial
+                {
+                    for (; userInt > 0; userInt--)
+                    {
+                        sum = sum * userInt;   //accumulate the sum of the iterations to find factorial.  First interation sets sum to userInt.  Covers rule 1! = 1
+                    }
+                    Console.WriteLine("The Factorial of " + userInput + " is " + sum);
+                    Console.ReadLine();
+                }
+
             }
-            else  //TryParse is false due to user entering a noninterger value
+            else  //Not an interger
             {
                 Console.WriteLine("You did not enter an integer!");
                 Console.ReadLine();
--- Homework3A/Homework3A/Program.cs
+++ Homework3A/Homework3A/Program.cs
@@ -6,10 +6,46 @@ using System.Threading.Tasks;
 
 namespace Homework3A
 {
-    class Program
+    class FindFactorial
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Please enter the integer you would like to calculate the factorial for...");
+
+            string userInput = Console.ReadLine();  //intilize userEntry with user kybd input
+            int userInt;  //Storage for parse value of userEntry
+            int sum = 1;  //Storage for partial result of the iterative factorial calculation.
+
+            if (int.TryParse(userInput, out userInt)) //if string parses to an int:  store in userInt
+            {
+                if (userInt == 0) //Check for 0
+                {
+                    Console.WriteLine("The Factorial of 0 is defined as 1");
+                    Console.ReadLine();
+                }
+
+                else if (userInt < 0)  //check for negative integer
+                {
+                    Console.WriteLine("The factorial of a negative integer is undefined");
+                    Console.ReadLine();
+                }
+
+                else //positive integer!!!  Find factorial
+                {
+                    for (; userInt > 0; userInt--)
+                    {
+                        sum = sum * userInt;   //accumulate the sum of the iterations to find factorial.  First interation sets sum to userInt.  Covers rule 1! = 1
+                    }
+                    Console.WriteLine("The Factorial of " + userInput + " is " + sum);
+                    Console.ReadLine();
+                }
+
+            }
+            else  //Not an interger
+            {
+                Console.WriteLine("You did not enter an integer!");
+                Console.ReadLine();
+            }
         }
     }
 }
